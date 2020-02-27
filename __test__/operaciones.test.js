af4408b866bb98d517e98cfd97e369f6e4dd85e4
ruddycondori <rucocool@gmail.com>
2020-02-27 15:10:56 -0400

    configurando el watch para las pruebas

--- __test__/operaciones.test.js
+++ __test__/operaciones.test.js
@@ -1,5 +1,13 @@
-import { suma } from '../operaciones';
+import { suma, resta } from '../operaciones';
 
 test('sumar 1 + 2 es igual a 3', () => {
   expect(suma(1, 2)).toBe(3);
+});
+
+describe('Probando Operaciones', () => {
+  
+  test('resta de elementos ', () => {
+      expect(resta(100, 60)).toEqual(40);
+  });
+
 });
--- operaciones.js
+++ operaciones.js
@@ -5,3 +5,7 @@ export const suma = (a, b) => {
 export const multiplicacion = (a, b) => {
     return a * b;
 }
+
+export const resta = (a, b) => {
+    return a - b;
+}
