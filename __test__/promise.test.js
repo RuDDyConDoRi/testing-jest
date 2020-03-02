fb46bd01af986e1c54ac056ccc9d61b724a53f7d
ruddycondori <rucocool@gmail.com>
2020-03-02 13:55:55 -0400

    test de promises - peticiones apis externas

---
+++ __test__/promise.test.js
@@ -0,0 +1,14 @@
+import { getDataFromApi } from '../promise';
+
+describe('Probando promesas!!! ', () => {
+    test('Realizando una peticion a una API', done => {
+
+        const api = 'http://localhost:3000/employees'
+
+        getDataFromApi(api).then( data => {
+            console.log(data);
+            expect(data.length).toBeGreaterThan(0);
+            done();
+        })       
+    });
+});
