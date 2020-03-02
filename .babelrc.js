42c2c4f904cc4262b8d725f2c66c0425ffc9e199
ruddycondori <rucocool@gmail.com>
2020-03-02 14:57:40 -0400

    modificando configuracion de babel (para usar async/await)

---
+++ .babelrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // See https://babeljs.io/docs/en/babel-preset-env#targets
+    presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+};
+
+/*
+config para el archivo ".babelrc"
+{
+    "presets": ["@babel/preset-env"]
+}
+*/
